a89495ac3661253a1a1f31949380c69c0b434417
Matthew Williams <m.williams@me.com>
2013-04-09 22:23:55 +0100

    Initial commit

---
+++ lib/revue.rb
@@ -0,0 +1,34 @@
+require 'trollop'
+require 'rest-client'
+require 'json'
+
+PROTOCOL = "https"
+URL = "#{PROTOCOL}://revue.io"
+PATH = "/api/v1"
+
+opts = Trollop::options do
+  version "revue 1.0.0 (c) 2013 Matthew Williams"
+  banner <<-EOS
+revue is a tool for creating code reviews from a diff file.
+
+Usage:
+
+revue <diff_file>
+
+where [options] are:
+EOS
+
+  opt :url, "Use an alternative revue instance, e.g. a local installation", short: "-u", default: URL
+  opt :verbose, "Make the program more talkative", short: "-v"
+  opt :version, "Print version and exit", short: "-V"
+end
+
+RestClient.proxy = ENV["#{PROTOCOL}_proxy"] if ENV["#{PROTOCOL}_proxy"]
+RestClient.log = STDOUT if opts[:verbose]
+begin
+  response = RestClient.post "#{opts[:url]}/#{PATH}", { 'content' => ARGF.read }.to_json, content_type: :json, accept: :json
+  puts JSON.parse(response)['url']
+rescue => e
+  STDERR.puts JSON.parse(e.response)["errors"]
+  exit 1
+end
---
+++ revue.gemspec
@@ -0,0 +1,15 @@
+Gem::Specification.new do |s|
+  s.name        = 'revue'
+  s.version     = '1.0.0'
+  s.date        = '2013-04-02'
+  s.summary     = "Command line tool for accessing http://revue.io"
+  s.description = "A gem for creating and fetching code reviews via the revue.io website"
+  s.authors     = ["Matthew Williams"]
+  s.email       = 'm.williams@me.com'
+  s.files       = ["lib/revue.rb"]
+  s.executables << 'revue'
+  s.add_dependency "rest-client", "~> 1.6.7"
+  s.add_dependency "trollop", "~> 2.0"
+  s.homepage    =
+    'http://rubygems.org/gems/revue'
+end
